cefd6b710dbc7b1c7fcbccb26350e057f1b87b77
kanchankhemnar <kanchankhemnar170@gmail.com>
2024-07-09 01:08:03 +0530

    step 2

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const fs=require('fs'); 
+const { log } = require('console');
 app.use(express.static(path.join(__dirname,'public')));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
 app.set("view engine","ejs");
 app.get("/",(req,res)=>{
-  res.render('index');
+  fs.readdir(`./files`,(err,files)=>{
+    res.render('index',{files:files});
+  })
 });
+
+app.post("/create",(req,res)=>{
+  fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`,req.body.details,(err)=>{
+      res.redirect("/");
+  });
+})
 app.listen(3000);
